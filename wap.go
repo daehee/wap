cd566b809897e0cb3811920b10043437c4501579
Daehee Park <hello@daehee.com>
2020-09-23 13:23:44 -0700

    strip confidence level metadata from fingerprint values

--- wap.go
+++ wap.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"sort"
 	"strconv"
+	"strings"
 )
 
 const wapSourceURL = "https://raw.githubusercontent.com/AliasIO/Wappalyzer/master/src/technologies.json"
@@ -113,67 +114,35 @@ func parseWapJSON(b []byte) (*Wappalyzer, error) {
 				}
 				parseCats(jz, fp, &wap.Categories)
 			case "certIssuer":
-				fp.CertIssuer = stringSlicer(jv)
+				fp.CertIssuer = wapStringSlice(jv)
 			case "cookies":
-				var jz map[string]string
-				if err := json.Unmarshal(jv, &jz); err != nil {
-					return nil, err
-				}
-				fp.Cookies = jz
+				fp.Cookies = wapMap(jv)
 			case "cpe":
-				var jz string
-				if err := json.Unmarshal(jv, &jz); err != nil {
-					return nil, err
-				}
-				fp.Cpe = jz
+				fp.Cpe = wapString(jv)
 			case "css":
-				fp.Css = stringSlicer(jv)
+				fp.Css = wapStringSlice(jv)
 			case "excludes":
-				fp.Excludes = stringSlicer(jv)
+				fp.Excludes = wapStringSlice(jv)
 			case "headers":
-				var jz map[string]string
-				if err := json.Unmarshal(jv, &jz); err != nil {
-					return nil, err
-				}
-				fp.Headers = jz
+				fp.Headers = wapMap(jv)
 			case "html":
-				fp.HTML = stringSlicer(jv)
+				fp.HTML = wapStringSlice(jv)
 			case "icon":
-				var jz string
-				if err := json.Unmarshal(jv, &jz); err != nil {
-					return nil, err
-				}
-				fp.Icon = jz
+				fp.Icon = wapString(jv)
 			case "implies":
-				fp.Excludes = stringSlicer(jv)
+				fp.Excludes = wapStringSlice(jv)
 			case "js":
-				var jz map[string]string
-				if err := json.Unmarshal(jv, &jz); err != nil {
-					return nil, err
-				}
-				fp.Js = jz
+				fp.Js = wapMap(jv)
 			case "meta":
-				var jz map[string]string
-				if err := json.Unmarshal(jv, &jz); err != nil {
-					return nil, err
-				}
-				fp.Meta = jz
+				fp.Meta = wapMap(jv)
 			case "robots":
-				fp.Robots = stringSlicer(jv)
+				fp.Robots = wapStringSlice(jv)
 			case "scripts":
-				fp.Scripts = stringSlicer(jv)
+				fp.Scripts = wapStringSlice(jv)
 			case "url":
-				var jz string
-				if err := json.Unmarshal(jv, &jz); err != nil {
-					return nil, err
-				}
-				fp.URL = jz
+				fp.URL = wapString(jv)
 			case "website":
-				var jz string
-				if err := json.Unmarshal(jv, &jz); err != nil {
-					return nil, err
-				}
-				fp.Website = jz
+				fp.Website = wapString(jv)
 			}
 		}
 		wap.Fingerprints = append(wap.Fingerprints, fp)
@@ -181,7 +150,7 @@ func parseWapJSON(b []byte) (*Wappalyzer, error) {
 	return wap, nil
 }
 
-func stringSlicer(v json.RawMessage) []string {
+func wapStringSlice(v json.RawMessage) []string {
 	var u interface{}
 	if err := json.Unmarshal(v, &u); err != nil {
 		log.Printf("%v: %+v", err, v)
@@ -189,11 +158,38 @@ func stringSlicer(v json.RawMessage) []string {
 	var ss []string
 	switch s := u.(type) {
 	case string:
-		ss = append(ss, s)
+		ss = append(ss, cleanWapMeta(s))
 	case []string:
+		for i, v := range s {
+			s[i] = cleanWapMeta(v)
+		}
 		return s
 	}
 	return ss
+}
+
+func wapString(v json.RawMessage) string {
+	var s string
+	if err := json.Unmarshal(v, &s); err != nil {
+		log.Printf("%v: %+v", err, v)
+	}
+	return cleanWapMeta(s)
+}
+
+func wapMap(v json.RawMessage) map[string]string {
+	var m map[string]string
+	if err := json.Unmarshal(v, &m); err != nil {
+		log.Printf("%v: %+v", err, v)
+	}
+	for k, v := range m {
+		m[k] = cleanWapMeta(v)
+	}
+	return m
+}
+
+func cleanWapMeta(s string) string {
+	split := strings.Split(s, "\\;")
+	return split[0]
 }
 
 func parseCats(src []int, dst *Fingerprint, categoriesCatalog *map[string]*Category) {
